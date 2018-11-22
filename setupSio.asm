4896bc101374cc96db9289537e079d5ec6036233
Andreas Eriksson Järliden <andreas.eriksson@elekta.com>
2018-11-22 21:19:32 +0100

    Disable SIO Auto Enable in setupSIO which prevented SIO input

--- setupSio.asm
+++ setupSio.asm
@@ -10,7 +10,7 @@ setupSio:
 	; Generate 9600 Hz from 4 MHz clock. 4M/(16*26)~=9615 Hz
 	; 7=0 disable interrupts, 6=0 TIMER mode, 5=0 16 PRESCALER,  4=1 POS EDGE
 	; 3=0 AUTO Start, 2=1 TIME Constant follows, 1=0 Continue, 0 = 1
-	; 1001 0101 = B5
+	; 0001 0101 = 15
 	ld a, 015h
 	out (CTC0)
 	; Following time constant: set counter to 26
@@ -26,7 +26,7 @@ setupSio:
 	ld a, 18h
 	out (SIO_B_CONTROL)
 
-	; Reset ext/status interrupts, Async mode, Parity, Stop bits
+	; WR4: Reset ext/status interrupts, Async mode, Parity, Stop bits
 	ld a, 14h
 	out (SIO_B_CONTROL)
 	; x1 clock External sync, No parity, 1 stop bit,
@@ -34,14 +34,14 @@ setupSio:
 	ld a, 04h
 	out (SIO_B_CONTROL)
 
-	; WR3: 11 8 bits, 1 No Auto Enable, 0000, 1 enable Rx: 1100 0001
+	; WR3: 11 8 bits, 0 No Auto Enable, 0000, 1 enable Rx: 1100 0001
 	ld a, 03h
 	out (SIO_B_CONTROL)
-	ld a, 0e1h
+	ld a, 0c1h
 	out (SIO_B_CONTROL)
 
 	; WR5: 0 DTR active, 11 8 bits, 0 no break, 1 Tx Enable, 0, 0 RTS, 0
-	; 1110 1010
+	; 0110 1000
 	ld a, 05h
 	out (SIO_B_CONTROL)
 	ld a, 068h
